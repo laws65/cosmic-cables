1514b3066317cfe3962273aa6f32b1e4c4172e7d
laws65 <seanes118@gmail.com>
2022-04-09 19:19:30 +0930

    bullet detecting collision with asteroid and effect

--- src/energy_ball/energy_ball.gd
+++ src/energy_ball/energy_ball.gd
@@ -8,10 +8,12 @@ func _ready() -> void:
 	$Tween.interpolate_property(self, "speed", speed, speed*10, 1.0, Tween.TRANS_EXPO, Tween.EASE_IN)
 	$Tween.interpolate_property(self, "scale", Vector2(0.8, 0.8), Vector2.ONE, 0.5, Tween.TRANS_BOUNCE, Tween.EASE_IN_OUT)
 	$Tween.start()
-	$CPUParticles2D.emitting = true
+	$Fired.emitting = true
 
 
 func _physics_process(delta: float) -> void:
 	var collision := move_and_collide(transform.x * speed * delta)
 	if collision != null:
-		pass
+		$Impact.global_rotation_degrees = rotation_degrees - 180
+		$Impact.global_position = collision.position
+		$AnimationPlayer.play("impact")
