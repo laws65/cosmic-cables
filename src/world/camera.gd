bda546ca164e9797b9b095adcf27dae598e46bdf
laws65 <seanes118@gmail.com>
2022-05-21 22:32:14 +0930

    camera shake on take damage

--- src/world/camera.gd
+++ src/world/camera.gd
@@ -1,19 +1,32 @@
 extends Camera2D
 
 
+export var decay = 0.8  # How quickly the shaking stops [0, 1].
+export var max_offset = Vector2(0.6, 0.3)  # Maximum hor/ver shake in pixels.
+
+var trauma = 0.0  # Current shake strength.
+var trauma_power = 2  # Trauma exponent. Use [2, 3].
+
+
 var target: Node2D
 var lerp_speed := 30.0
 
 var safe_spot = 0.2
 var offset_lerp_speed := 15.0
+var shake_offset := Vector2.ZERO
 
 # ideally, should be in process
 # but causes massive jittering
 # so idk what's going on
 func _physics_process(delta: float) -> void:
-	if is_instance_valid(target):
-		position = lerp(position, target.position, lerp_speed * delta)
+	track_target(delta)
+	soldat_style_camera_offsetting(delta)
+	if trauma:
+		trauma = max(trauma - decay * delta, 0)
+		shake()
 
+
+func soldat_style_camera_offsetting(delta: float) -> void:
 	var screen_size := get_viewport_rect().size
 	var mouse_screen_pos := get_viewport().get_mouse_position()
 	var new_offset := (mouse_screen_pos / screen_size) - Vector2(0.5, 0.5)
@@ -28,7 +41,25 @@ func _physics_process(delta: float) -> void:
 	else:
 		new_offset.y -= sign(new_offset.y) * safe_spot
 
-	offset = lerp(offset, new_offset, offset_lerp_speed * delta)
+	offset = offset.linear_interpolate(new_offset, offset_lerp_speed * delta)
 	# engine bug :)
 	offset_h = offset.x
 	offset_v = offset.y
+	
+	# apply shake offset
+	offset += shake_offset
+
+
+func track_target(delta: float) -> void:
+	if is_instance_valid(target):
+		position = lerp(position, target.position, lerp_speed * delta)
+
+
+func add_trauma(amount):
+	trauma = min(trauma + amount, 1.0)
+
+
+func shake():
+	var amount = pow(trauma, trauma_power)
+	shake_offset.x = max_offset.x * amount * rand_range(-0.5, 0.5)
+	shake_offset.y = max_offset.y * amount * rand_range(-0.5, 0.5)
