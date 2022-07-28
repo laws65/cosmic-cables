e837cb58c34352b8372a04ce976545ddffd80531
laws65 <seanes118@gmail.com>
2022-07-28 18:44:42 +0930

    fix placing, fix error

--- src/buildings/place_probe.gd
+++ src/buildings/place_probe.gd
@@ -12,7 +12,7 @@ func _ready() -> void:
 	SignalBus.connect("toolbar_item_building_setup", self, "build_display_for")
 
 
-func _unhandled_input(event: InputEvent) -> void:
+func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("place"):
 		if can_build():
 			owner.build_building(building_info, global_position)
--- src/ui/ui.gd
+++ src/ui/ui.gd
@@ -1,13 +1,1 @@
 extends Control
-tool
-
-func _ready() -> void:
-	print_children_if_mouse_mode_stop(self)
-
-
-func print_children_if_mouse_mode_stop(n):
-	#n.mouse_filter = MOUSE_FILTER_PASS
-	if n.mouse_filter == MOUSE_FILTER_STOP:
-		print(n.name + "yep, parent " + str(n.get_parent()))
-	for i in n.get_children():
-		print_children_if_mouse_mode_stop(i)
