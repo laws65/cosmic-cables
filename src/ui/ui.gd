a939f788f5bbc674d455f607f82f39fe0fe139fc
laws65 <seanes118@gmail.com>
2022-07-29 22:43:11 +0930

    fix the bullshit

--- src/ui/ui.gd
+++ src/ui/ui.gd
@@ -1,16 +1,1 @@
 extends Control
-tool
-
-
-func _ready() -> void:
-	yes(self)
-
-
-func yes(node):
-	if "mouse_filter" in node:
-		if node.mouse_filter == MOUSE_FILTER_STOP:
-			node.mouse_filter = MOUSE_FILTER_IGNORE
-			print(node.get_parent().name + " | " + node.name)
-
-	for c in node.get_children():
-		yes(c)
