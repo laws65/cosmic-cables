893ddf691dcf4ba0a6a40906eec2672e3fa07dc5
laws65 <seanes118@gmail.com>
2022-08-04 23:18:35 +0930

    wave check

--- src/world/enemy_spawner.gd
+++ src/world/enemy_spawner.gd
@@ -2,8 +2,9 @@ extends Node
 
 
 var old_unobtainium_amount := 0
-var time := 5.0 * 60.0 * 0.05
-
+var time := 5.0 * 60.0
+var waves_done := 0
+var wave_size := 1
 
 func _ready() -> void:
 	Game.connect("unobtainium_changed", self, "_on_Unobtainium_changed")
@@ -17,13 +18,20 @@ func _physics_process(delta: float) -> void:
 
 
 func _spawn_new_enemy() -> void:
+	waves_done += 1
 	var player := get_tree().get_nodes_in_group("player").front() as Player
-	var enemy_position := player.position + (Vector2.RIGHT * 200).rotated(player.global_rotation)
-	var enemy_instance = load("res://src/enemy_ship/enemy_ship.tscn").instance()
-	enemy_instance.position = enemy_position
-	enemy_instance.target = player
-	owner.add_child(enemy_instance)
-	Game.add_message_popup("Enemy incoming!!")
+	for _i in wave_size:
+		var enemy_position := player.position + (Vector2.RIGHT * 400).rotated(player.global_rotation)
+		var enemy_instance = load("res://src/enemy_ship/enemy_ship.tscn").instance()
+		enemy_instance.position = enemy_position
+		enemy_instance.target = player
+		owner.add_child(enemy_instance)
+	if wave_size == 1:
+		Game.add_message_popup("Enemy incoming!!")
+	else:
+		Game.add_message_popup("Enemies incoming!! (%s)" % wave_size)
+	if waves_done % 4 == 0:
+		wave_size += 1
 
 
 func _on_Unobtainium_changed(new_amount: int) -> void:
